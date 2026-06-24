9d675b9ad9c7432d507c6b1727d18174d9720663
Taniel Rémi <taniel.remi@gmail.com>
2026-06-24 13:53:22 +0200

    🌐 localize portfolio content to French
    All user-facing text and data are translated into French to provide a fully localized experience, alongside minor layout and styling adjustments.

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,4 +1,5 @@
-export const APP_NAME = 'iAmPinpin - Remi Taniel';
+export const APP_NAME = 'Remi Taniel / Portfolio';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/le-pinpin/';
 export const GITHUB_URL = 'https://github.com/Synthx';
 export const EMAIL_URL = 'mailto:pinpin@jetdev.fr';
+export const LOCALE = 'fr-FR';
